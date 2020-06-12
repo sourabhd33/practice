0cb42b5d35bbb9229754cf3fa347eb690fc7b633
sourabhd33 <66833366+sourabhd33@users.noreply.github.com>
2020-06-12 18:54:36 +0530

    mainfiles

---
+++ Makefile
@@ -0,0 +1,2 @@
+hello_exec: hello.c
+	g++ hello.c -o hello_exec
---
+++ hello.c
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <stdio.h>
+#include <conio.h>
+
+int main(void) {
+  printf("Hello World\n");
+  printf("Hello Jenkins\n");
+  printf("I have succssfully built abd run\n");
+  return 0;
+}
